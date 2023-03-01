9c58e8c1da1978270e57554a23a8a98ecab754b1
Lukas Bombach <lukas.bombach@googlemail.com>
2023-03-01 12:05:24 +0100

    get css usages

--- index.ts
+++ index.ts
@@ -1,15 +1,36 @@
 import { Project, ts } from "ts-morph";
 
+import type { Node } from "ts-morph";
+
 const project = new Project({
   tsConfigFilePath: "./tsconfig.json",
 });
 const sourceFile = project.getSourceFileOrThrow("fixtures/ArticleBody.style.tsx");
 
 const emotionReact = sourceFile.getImportDeclaration("@emotion/react");
+
+assertNonNullable(emotionReact);
+
 const namedImports = emotionReact.getNamedImports();
 const [probablyCss] = namedImports;
 
-const usages = probablyCss.getNameNode().findReferencesAsNodes();
-console.log(usages);
+const usages = probablyCss
+  .getNameNode()
+  .findReferencesAsNodes()
+  .map(n => n.getParent())
+  .filter(isNonNullable)
+  .filter(isTaggedTemplateExpression);
 
-debugger;
+console.log(usages.map(p => p?.getText()));
+
+function isTaggedTemplateExpression(node: Node<ts.Node>): node is Node<ts.TaggedTemplateExpression> {
+  return ts.isTaggedTemplateExpression(node.compilerNode);
+}
+
+function isNonNullable<T>(value: T | undefined | null): value is T {
+  return typeof value !== "undefined" && value !== null;
+}
+
+function assertNonNullable<T>(value: T | undefined | null): asserts value is T {
+  if (!isNonNullable(value)) throw new Error(`Value must not be ${value}`);
+}
