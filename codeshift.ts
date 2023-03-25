e43706250e0bfefba1022c55a513cae61d7be75d
Lukas Bombach <lukas.bombach@googlemail.com>
2023-03-25 14:48:50 +0100

    wip

--- codeshift.ts
+++ codeshift.ts
@@ -2,12 +2,8 @@ import type { API, FileInfo } from "jscodeshift";
 
 export default function transformer(file: FileInfo, api: API) {
   const j = api.jscodeshift;
-  const root = j(file.source);
 
-  root.
-
-  const variableDeclarators = root.findVariableDeclarators("foo");
-  variableDeclarators.renameTo("bar");
+  const cssImport = j(file.source).find(j.ImportDeclaration, node => node.source.value === "@emotion/react");
 
   return root.toSource();
 }
